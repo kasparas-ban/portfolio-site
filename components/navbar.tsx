1c78facd6ca8d781e10d0d36bdfb7aa3bb3f881f
kasparas-ban <kaspis245@gmail.com>
2023-06-10 10:23:27 +0300

    Initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+const navItems = {
+  '/': {
+    name: 'home',
+  },
+  '/resume': {
+    name: 'resume',
+  },
+  '/projects': {
+    name: 'projects',
+  },
+}
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row gap-3 bg-red-400 max-md:justify-center md:flex-col">
+      <Link href="/" className="block">
+        Home
+      </Link>
+      <Link href="/resume" className="block">
+        Resume
+      </Link>
+      <Link href="/projects" className="block">
+        Projects
+      </Link>
+    </nav>
+  )
+}
