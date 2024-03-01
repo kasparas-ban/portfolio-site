781461cf5eaaba00ebf7da9235cc2c7be649754a
kasparas-ban <kasparas.baniulis@gmail.com>
2024-03-01 16:06:15 +0200

    Fixed links

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -84,6 +84,7 @@ function GithubButton({
           'relative flex cursor-pointer items-center justify-center rounded-md py-1 text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100',
           isPrivate && 'pointer-events-none'
         )}
+        href={link}
       >
         <div>
           <svg
